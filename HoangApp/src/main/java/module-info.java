c378aa38d656a64ce8bc32d590e6de9dda90a9ad
hgooshvhd123@gmail.com <hgooshvhd123@gmail.com>
2023-02-17 15:51:22 +0700

    test

---
+++ HoangApp/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module nhom7.hoangapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens nhom7.hoangapp to javafx.fxml;
+    exports nhom7.hoangapp;
+}
---
+++ HoangApp/src/main/java/nhom7/hoangapp/SecondaryController.java
@@ -0,0 +1,12 @@
+package nhom7.hoangapp;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
